78ff1f94190a7e9e6b9edd6a1280ef939a0d4b81
Fenev EA <fenevegor@gmail.com>
2023-07-03 22:41:35 +0300

    1 commit

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int operand1 = scanner.nextInt();
+        char operation = scanner.next().charAt(0);
+        int operand2 = scanner.nextInt();
+        int result = 0;
+        switch (operation) {
+            case '+':
+                result = operand1 + operand2;
+                break;
+            case '-':
+                result = operand1 - operand2;
+                break;
+            case '*':
+                result = operand1 * operand2;
+                break;
+            case '/':
+                result = operand1 / operand2;
+                break;
+            default:
+                System.out.println("Введен некорректный оператор");
+        }
+        System.out.println(result);
+    }
+}
